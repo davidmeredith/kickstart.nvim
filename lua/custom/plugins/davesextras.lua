0396bb5ae4e53f586cdec7fbc0297e4b22253490
daveM <david.meredith@stfc.ac.uk>
2023-04-22 11:50:57 +0100

    Re-enabling nvim-surround

--- lua/custom/plugins/davesextras.lua
+++ lua/custom/plugins/davesextras.lua
@@ -42,14 +42,14 @@ return {
     --    'change quot*es'            cs'"            "change quotes"
     --    <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
     --    delete(functi*on calls)     dsf             function calls
-    -- {
-    --     "kylechui/nvim-surround",
-    --     version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    --     event = "VeryLazy",
-    --     config = function()
-    --         require("nvim-surround").setup({
-    --             -- Configuration here, or leave empty to use defaults
-    --         })
-    --     end
-    -- }
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    }
 }
