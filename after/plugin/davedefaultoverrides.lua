3e2b4240de5ab5c31652ac6a6055002c0c2d39f7
daveM <david.meredith@stfc.ac.uk>
2023-12-21 10:27:39 +0000

    Set tabstop,shiftwidth overrides

--- after/plugin/davedefaultoverrides.lua
+++ after/plugin/davedefaultoverrides.lua
@@ -5,6 +5,12 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+
 -- Decrease update time
 vim.o.updatetime = 250
 --
